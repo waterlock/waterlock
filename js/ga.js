f3868cc112315277bee022f6f516e7b53c20e21d
David Rivera <David.R.Rivera193@gmail.com>
2014-06-23 22:35:38 -0400

    ga

--- js/ga.js
+++ js/ga.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-37873083-2', 'davidrivera.github.io');
+  ga('create', 'UA-37873083-2', 'waterlock.ninja');
   ga('send', 'pageview');
