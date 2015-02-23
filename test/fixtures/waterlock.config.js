430b8cbe851aed583d7796486dfc34c5e4e86652
wayne-o <wayne@prjatk.com>
2015-02-23 17:53:43 +0000

    adding registerSuccess cycle event

--- test/fixtures/waterlock.config.js
+++ test/fixtures/waterlock.config.js
@@ -5,20 +5,20 @@
  *
  * defines various options used by waterlock
  * for more informaiton checkout
- * 
+ *
  * http://waterlock.ninja/documentation
  */
 module.exports.waterlock = {
-  
+
   // Base URL
-  // 
+  //
   // used by auth methods for callback URI's using oauth and for password
   // reset links.
   baseUrl: 'http://localhost:1337',
-  
-  // Auth Method(s) 
-  // 
-  // this can be a single string, an object, or an array of objects for your 
+
+  // Auth Method(s)
+  //
+  // this can be a single string, an object, or an array of objects for your
   // chosen auth method(s) you will need to see the individual module's README
   // file for more information on the attributes necessary. This is an example
   // of the local authentication method with password reset tokens disabled.
@@ -39,7 +39,7 @@ module.exports.waterlock = {
           from: 'no-reply@domain.com',
           subject: 'Your password reset!',
           forwardUrl: 'http://localhost:1337'
-        },  
+        },
         template:{
           file: '../views/email.jade',
           vars:{}
@@ -50,8 +50,8 @@ module.exports.waterlock = {
 
   // JSON Web Tokens
   //
-  // this provides waterlock with basic information to build your tokens, 
-  // these tokens are used for authentication, password reset, 
+  // this provides waterlock with basic information to build your tokens,
+  // these tokens are used for authentication, password reset,
   // and anything else you can imagine
   jsonWebTokens:{
 
@@ -73,7 +73,7 @@ module.exports.waterlock = {
     login: {
 
       // This can be any one of the following
-      // 
+      //
       // url - 'http://example.com'
       // relativePath - '/blog/post'
       // obj - {controller: 'blog', action: 'post'}
@@ -82,7 +82,7 @@ module.exports.waterlock = {
       success: 'default',
 
       // This can be any one of the following
-      // 
+      //
       // url - 'http://example.com'
       // relativePath - '/blog/post'
       // obj - {controller: 'blog', action: 'post'}
@@ -95,7 +95,7 @@ module.exports.waterlock = {
     logout: {
 
       // This can be any one of the following
-      // 
+      //
       // url - 'http://example.com'
       // relativePath - '/blog/post'
       // obj - {controller: 'blog', action: 'post'}
@@ -104,7 +104,26 @@ module.exports.waterlock = {
       success: 'default',
 
       // This can be any one of the following
-      // 
+      //
+      // url - 'http://example.com'
+      // relativePath - '/blog/post'
+      // obj - {controller: 'blog', action: 'post'}
+      // string - 'custom json response string'
+      // default - 'default'
+      failure: 'default'
+    },
+    // post register event
+    register: {
+      // This can be any one of the following
+      //
+      // url - 'http://example.com'
+      // relativePath - '/blog/post'
+      // obj - {controller: 'blog', action: 'post'}
+      // string - 'custom json response string'
+      // default - 'default'
+      success: 'default',
+      // This can be any one of the following
+      //
       // url - 'http://example.com'
       // relativePath - '/blog/post'
       // obj - {controller: 'blog', action: 'post'}
